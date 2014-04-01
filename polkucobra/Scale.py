bf367a9ea3de99e05ea8e5958f1e181572cdb3d8
Valtteri Wikström <valtteri.wikstrom@aalto.fi>
2014-04-01 16:49:26 +0300

    python&pd

---
+++ polkucobra/Scale.py
@@ -0,0 +1,156 @@
+import serial
+from numpy import mean
+from numpy import inf
+import time
+
+class Scale:
+	def __init__(self):
+		self.port = serial.Serial("/dev/tty.usbmodemfd121", 57600)
+		#self.port = serial.Serial("/dev/tty.usbmodemfd1221", 57600)
+
+		#constant variables:
+		self.nScales = 6
+		self.nPrevValues = 200
+		self.minCalibratedScaleVal = 0.1
+		self.maxIdDiff = 0.1
+		self.nPrevIds = 6
+		self.nIds = 6
+		self.stableRange = 2
+		self.stableTolerance = 50
+		self.colors = [ [120, 0, 0], [0, 120, 0], [0, 0, 120], [60, 60, 0], [60, 0, 60], [0, 60, 60] ]
+		
+		#class variables:
+		#self.iCurrVal = 0
+		self.iCurrId = 0
+		self.allScales = [ [0] * self.nPrevValues for i in range(self.nScales) ]
+		self.calibratedScales = [ [0] * self.nPrevValues for i in range(self.nScales) ]
+		self.filteredScales = [ [0] * self.nPrevValues for i in range(self.nScales) ]
+		self.allIds = [inf] * self.nPrevIds
+		#self.allIds = [0.8, 0.9, 1.0, 1.1, 1.2, 1.3] 
+		self.idsAtScales = [-1] * self.nScales
+		#calibration variables:
+		self.calibratingScale = -1
+		self.minScales = [0] * self.nScales
+		self.calibrationScales = [1] * self.nScales
+		self.calibrated = False
+			
+	
+	def updateData ( self, incomingData ):
+		for (index, value) in enumerate(incomingData):
+			self.allScales[index].pop(0)
+			self.allScales[index].append(value)
+			self.calibratedScales[index].pop(0)
+			self.calibratedScales[index].append( self.getCalibratedData(index, value) )
+			
+			if(self.calibrated):
+				id = self.getId( index )
+				self.idsAtScales[index] = id
+				#print(id, self.allIds[id], end="\t")
+		#print()
+			
+	def getId (self, index ):
+		if ( self.idsAtScales[index] == -1 and min( self.calibratedScales[index] ) >= self.minCalibratedScaleVal ):
+			print("getting id")
+			id = -1
+			calibratedValue = self.calibratedScales[index][-1]
+			mindiff = inf
+			for (i, idValue) in enumerate(self.allIds):
+				diff = abs(calibratedValue - idValue)
+				if ( diff < self.maxIdDiff and diff < mindiff):
+					mindiff = diff
+					id = i
+			if (id == -1):
+				self.allIds[self.iCurrId] = calibratedValue
+				id = self.iCurrId
+				self.iCurrId = (self.iCurrId + 1) % self.nPrevIds
+			else:
+				self.allIds[id] = calibratedValue
+			return (id % self.nIds)
+		elif ( min(self.calibratedScales[index]) < self.minCalibratedScaleVal ):
+			return -1
+		else:
+			return self.idsAtScales[index]
+				
+	def getCalibratedData( self, index, value ):
+		return float(value - self.minScales[index]) / float(self.calibrationScales[index] - self.minScales[index])
+					
+
+	def parseSerialData(self):
+		valArr = []
+		try:
+			string = self.port.readline().decode('ascii')
+			strArr = string.split(',')
+			for subStr in strArr:
+				val = int(subStr)
+				valArr.append(val)
+		except:
+			print ("didn't parse value")
+		return valArr
+
+	def update (self):
+		newValues = self.parseSerialData()
+		if len(newValues) == self.nScales:
+			self.updateData(newValues)
+		if not self.calibratingScale == -1:
+			self.calibrate()
+			
+	def changeColor ( self, index, color ):
+		msg = "I {0} {1} {2} {3}".format(index, color[0], color[1], color[2])
+		#print (msg)
+		self.port.write(msg.encode())
+		
+	def startCalibration ( self ):
+		self.calibratingScale = 0
+		self.minScales = [0] * self.nScales
+		self.changeColor(self.calibratingScale, [255, 0, 0])
+	
+	def calibrate ( self ):
+		if self.minScales[self.calibratingScale] == 0:
+			print ("Calibrating minimum")
+			self.minScales[self.calibratingScale] = max(self.allScales[self.calibratingScale])
+		elif min(self.allScales[self.calibratingScale]) > 2 * self.minScales[self.calibratingScale]:
+		# note, "2 * " in above function could be "other things"
+			print ("Calibrating")
+			self.calibrationScales[self.calibratingScale] = max(self.allScales[self.calibratingScale])
+			self.changeColor(self.calibratingScale, [0, 0, 0])
+			self.calibratingScale += 1
+			if self.calibratingScale == self.nScales:
+				self.calibratingScale = -1
+				print (self.calibrationScales)
+				print (self.minScales)
+				self.calibrated = True
+				self.saveCalibrationFile("calibration.txt")
+			else:
+				self.changeColor(self.calibratingScale, [255, 0, 0])
+			
+	def saveCalibrationFile( self, filename ):
+		outFile = open(filename, 'w')
+		for i in range(self.nScales):
+			outFile.write(str(self.calibrationScales[i]))
+			outFile.write(" ")
+		outFile.write('\n')
+		for i in range(self.nScales):
+			outFile.write(str(self.minScales[i]))
+			outFile.write(" ")
+		outFile.close()
+	
+	def loadCalibrationFile( self, filename ):
+		inFile = open(filename, 'r')
+		str = inFile.readline()
+		str2 = inFile.readline()
+		strArr = str.split(' ')
+		strArr2 = str2.split(' ')
+		for i in range(self.nScales):
+			self.calibrationScales[i] = int(strArr[i])
+			self.minScales[i] = int(strArr2[i])
+		self.calibrated = True
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
--- polkuduino/polkuduino.ino
+++ polkuduino/polkuduino.ino
@@ -4,8 +4,10 @@
 const int n_leds = 5;
 const int ledPins [] = { A10, A11, A12, A13, A14, A15 };
 const int scalePins [] = { A0, A1, A2, A3, A4, A5 };
-const float filter = 0.9;
-const int looptime = 50;
+const float filter = 0.0;
+//const float colorFilter = 0.97;
+const int colorSpeed = 2;
+const int looptime = 10;
 
 
 Adafruit_NeoPixel leds [] = { Adafruit_NeoPixel(n_leds, ledPins[0], NEO_GRB + NEO_KHZ800), 
@@ -16,13 +18,17 @@ Adafruit_NeoPixel leds [] = { Adafruit_NeoPixel(n_leds, ledPins[0], NEO_GRB + NE
                               Adafruit_NeoPixel(n_leds, ledPins[5], NEO_GRB + NEO_KHZ800) };
                               
 float readValue [] = { 0, 0, 0, 0, 0, 0 };
+int colors [6][3] = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
+int targetColors [6][3] = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
 
 void setup() {
   
   Serial.begin(57600);
+  Serial.setTimeout(1);
+  
+  //analogReference(INTERNAL2V56);
   
   for (int i=0; i < N_SCALES; i++) {
-    pinMode(scalePins[i], INPUT);
     
     leds[i].begin();
     leds[i].show(); // Initialize all pixels to 'off'
@@ -33,34 +39,63 @@ void setup() {
 void loop() {
   long timer = millis();
   
+  if (Serial.available() > 0) {
+    char prefix = Serial.read();
+    if(prefix == 'I') {
+      int leds_i = Serial.parseInt();
+      
+      int r = Serial.parseInt();
+      int g = Serial.parseInt();
+      int b = Serial.parseInt();
+      
+      
+      targetColors[leds_i][0] = r;
+      targetColors[leds_i][1] = g;
+      targetColors[leds_i][2] = b;
+    }
+  }
+
+  
+  
   for(int i=0; i < N_SCALES; i++) {
+  //for(int i=1; i < 2; i++) {
+    
+    /*
+    long readTime = millis();
+    
+    while (millis()-readTime < 1000) {
+      analogRead(scalePins[i]);
+      delay(10);
+    }*/
+    //analogRead(scalePins[i]);
+    //delay(10);
+    
     readValue[i] = readValue[i] * filter + analogRead(scalePins[i]) * (1.0 - filter);
     Serial.print(int(readValue[i]));
-    Serial.print("\t");
+    if(i != N_SCALES - 1) Serial.print(",");
+    
+    
+    for(int j = 0; j < 3; j++) {
+      int diff = colors[i][j] - targetColors[i][j];
+      if(abs(diff) < colorSpeed) {
+        colors[i][j] == targetColors[i][j];
+      } else if(diff < 0) {
+        colors[i][j] += colorSpeed;
+      } else if(diff > 0) {
+        colors[i][j] -= colorSpeed;
+      }
+      //colors[i][j] = colors[i][j] * colorFilter + targetColors[i][j] * (1.0 - colorFilter);
+    }
+    
     for(int j = 0; j < n_leds; j++) {
-      leds[i].setPixelColor(j, max(readValue[i]-100, 0)/2, 0, max(readValue[i]-100, 0)/2);
+      leds[i].setPixelColor(j, colors[i][0], colors[i][1], colors[i][2]);
     }
     leds[i].show();
     
   }
   Serial.println();
-  /*
-  if(Serial.available() > 0) {
-    char prefix = Serial.read();
-    if(prefix == 'I') {
-      int leds_i = Serial.parseInt();
-      int r = Serial.parseInt();
-      int g = Serial.parseInt();
-      int b = Serial.parseInt();
-      
-      for(int i = 0; i < n_leds; i++) {
-        strip.setPixelColor(i, r, g, b);
-      }
-      strip.show();
-    }
-  }
-  */
   
+  //Serial.println(millis()-timer);
   long time = millis() - timer;
   if(time < looptime) {
     delay(looptime-time);
